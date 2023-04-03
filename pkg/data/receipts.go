9345ff8b086153bec5e6144323e9f9681b87188b
mymacbook <1017962047@qq.com>
2023-04-03 22:19:15 +0800

    add event field

--- pkg/data/receipts.go
+++ pkg/data/receipts.go
@@ -2,10 +2,13 @@ package data
 
 // Event -
 type Event struct {
-	Order       uint64   `json:"order"`
-	FromAddress string   `json:"from_address"`
-	Keys        []string `json:"keys"`
-	Data        []string `json:"data"`
+	Order           uint64   `json:"order"`
+	FromAddress     string   `json:"from_address"`
+	Keys            []string `json:"keys"`
+	Data            []string `json:"data"`
+	BlockHash       string   `json:"block_hash"`
+	BlockNumber     uint64   `json:"block_number"`
+	TransactionHash string   `json:"transaction_hash"`
 }
 
 // Message -
